243866625f949f5523679198caee799d08978a4c
R3DDoS_GH0$T <gevorga2002@mail.ru>
2026-08-25 18:45:18 +0300

    Add files via upload

---
+++ bases/passwords_master/hidden_dirs.py
@@ -0,0 +1,43 @@
+black_dirs = (
+    "/.git", "/.git/config", "/.git/HEAD", "/.gitignore",
+    "/.svn", "/.svn/entries", "/.hg", "/.bzr",
+
+    "/.env", "/.env.local", "/.env.production",
+    "/config.php", "/config.yml", "/config.json",
+    "/settings.py", "/settings.json",
+
+    "/database.sql", "/db.sql", "/dump.sql",
+    "/backup.sql", "/backup.zip", "/backup.tar.gz",
+    "/data.db", "/database.db",
+
+    "/admin", "/admin.php", "/administrator",
+    "/login", "/login.php", "/signin",
+    "/dashboard", "/panel", "/control",
+
+    "/wp-admin", "/wp-login.php", "/wp-content",
+    "/wp-includes", "/wp-config.php",
+    "/phpmyadmin", "/pma", "/myadmin",
+    "/joomla/administrator", "/administrator/index.php",
+
+    "/.htaccess", "/.htpasswd",
+    "/server-status", "/server-info",
+    "/phpinfo.php", "/info.php", "/test.php",
+
+    "/logs", "/log", "/debug.log", "/error.log",
+    "/access.log", "/var/log",
+
+    "/robots.txt", "/sitemap.xml", "/readme.txt",
+    "/README.md", "/LICENSE", "/CHANGELOG.md",
+
+    "/api", "/api/v1", "/api/v2",
+    "/rst", "/graphql", "/swagger",
+
+    "/uploads", "/upload", "/files", "/downloads",
+    "/images", "/img", "/css", "/js",
+    "/assets", "/static", "/media",
+    "/old", "/new", "/test", "/dev",
+    "/tmp", "/temp", "/cache", "/backup",
+    "/private", "/secret", "/hidden",
+    "/debug", "/trace", "/error",
+    "/install", "/setup", "/update",
+)
